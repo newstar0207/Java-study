91ab84463a309a049c58b320954b59ece6419941
newstar0207 <newstar0207@naver.com>
2021-03-20 23:26:08 +0900

    파파고 api를 사용한 textconverter

---
+++ ch12/src/ch12/TextConverter2.java
@@ -0,0 +1,187 @@
+package ch12;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.WindowConstants;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+public class TextConverter2 extends JFrame implements ActionListener {
+    private JButton convertBtn;
+    private JButton cancelBtn;
+    private JTextArea textIn;
+    private JTextArea textOut;
+    private final String CLIENT_ID = "hXy_mcjHOognVTVvKMBN";
+    private final String CLIENT_SECRET = "FmxO7h0xxZ";
+    
+    // String 형 문자 하나하나 프린틓 하는 법 -> s.charAt(i);
+    // int idx = s.indexOf("백두산") - > 백두산이 시작되는 인덱스 번호
+    
+    public TextConverter2() {
+        super("텍스트 변환");
+
+        textIn = new JTextArea(10, 14);
+        textOut = new JTextArea(10, 14);
+        textIn.setLineWrap(true);
+        textOut.setLineWrap(true);
+        textOut.setEditable(false);
+
+        JPanel textAreaPanel = new JPanel(new GridLayout(1, 2, 20, 20));
+        textAreaPanel.add(textIn);
+        textAreaPanel.add(textOut);
+
+        convertBtn = new JButton("변환");
+        cancelBtn = new JButton("취소");
+        convertBtn.addActionListener(this);
+        cancelBtn.addActionListener(this);
+
+        JPanel btnPanel = new JPanel();
+        btnPanel.add(convertBtn);
+        btnPanel.add(cancelBtn);
+
+        JPanel mainPanel = new JPanel(new BorderLayout(10, 10));
+        mainPanel.add(textAreaPanel, BorderLayout.CENTER);
+        mainPanel.add(btnPanel, BorderLayout.SOUTH);
+
+        this.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 20));
+        this.add(mainPanel);
+        this.pack();
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == convertBtn) {
+            textOut.setText("");
+            String result = toEnglish(textIn.getText());  // result -> 변역된 결과임
+            result = JsonParser(result);
+            textOut.append(result);
+        }
+
+        if (e.getSource() == cancelBtn) {
+            textOut.setText("");
+        }
+    }
+    
+    public String JsonParser(String result) {
+    	JsonParser parser = new JsonParser();
+    	JsonElement element = parser.parse(result);
+    	if(element.getAsJsonObject().get("errorMessage") != null) {
+    		String transResult = ("errorcode : " + element.getAsJsonObject().get("errorCode").getAsString());
+    		return transResult;
+    	}else if(element.getAsJsonObject().get("message") != null) {
+    		String transResult = (element.getAsJsonObject().get("message").getAsJsonObject().get("result")
+    		.getAsJsonObject().get("translatedText").getAsString());
+    		return transResult;
+    	}else {
+    		return "error";
+    	}
+    }
+    
+    
+/////////새로추가한거
+    private String toEnglish(String korean) { // textIn 의 텍스트를 korean 으로 넣음
+        String result = korean;
+
+        // result = result.replace("텍스트", "text");
+        // result = result.replace("영어", "english");
+        String apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+        String text;
+        try {
+            text = URLEncoder.encode(korean, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("인코딩 실패", e);
+        }
+
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("X-Naver-Client-Id", CLIENT_ID);
+        requestHeaders.put("X-Naver-Client-Secret", CLIENT_SECRET);
+
+        result = post(apiURL, requestHeaders, text);
+//        result = result.substring(result.indexOf("translatedText") + "translatedText".length() + 3, result.indexOf("engineType") -3); // substring 을 이용하여 번역된 문장만 가져옴 / 원본은 바뀌지 않음
+        return result;
+    }
+
+    private static String post(String apiUrl, Map<String, String> requestHeaders, String text){
+        HttpURLConnection con = connect(apiUrl);
+        String postParams = "source=ko&target=en&text=" + text; //원본언어: 한국어 (ko) -> 목적언어: 영어 (en)
+        try {
+            con.setRequestMethod("POST");
+            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            con.setDoOutput(true);
+            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                wr.write(postParams.getBytes());
+                wr.flush();
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 응답
+                return readBody(con.getInputStream());
+            } else {  // 에러 응답
+                return readBody(con.getErrorStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+
+    private static HttpURLConnection connect(String apiUrl){
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection)url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+    private static String readBody(InputStream body){
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        new TextConverter2();
+    }
+
+}
