6f149388df3d3e0b9dba1d181c741cee8eb37661
newstar0207 <newstar0207@naver.com>
2021-04-13 00:30:00 +0900

    WatermelonWatermelon

--- ch12/src/tutoring/WatermelonWatermelon.java
+++ ch12/src/tutoring/WatermelonWatermelon.java
@@ -2,4 +2,20 @@ package tutoring;
 
 public class WatermelonWatermelon {
 
+	public static void main(String[] args) {
+		
+		int a = 6;
+		String resuit = "";
+		
+		for(int i = 0 ; i < a; i++) {
+			if(i % 2 == 0) {
+				resuit += "¼ö";
+			}else {
+				resuit += "¹Ú";
+			}
+		}
+		
+		System.out.println(resuit);
+	}
+
 }
